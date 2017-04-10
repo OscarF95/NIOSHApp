01191de9b5fcbe74f3fc774993595365103dde05
ErnestoSalazar <ernestoalbertosalazar@gmail.com>
2017-04-10 12:55:51 -0700

    First commit

---
+++ NIOSHApp/NIOSHApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIOSHApp.ViewModels
+{
+    class MainViewModel
+    {
+    }
+}
